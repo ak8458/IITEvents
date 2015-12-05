ab308a3583eef9b2bce8a3cd3750071a6c0f1071
ak8458 <ak8458@gmail.com>
2015-12-05 02:24:26 -0600

    [AP] added comments

--- app/src/main/java/iitevent/project/iit/activities/EventListViewActivity.java
+++ app/src/main/java/iitevent/project/iit/activities/EventListViewActivity.java
@@ -29,7 +29,8 @@ import iitevent.project.iit.helpers.JSONParser;
 import iitevent.project.iit.helpers.ListViewAdapter;
 
 /**
- * Created by demo on 21-11-2015.
+ * Created by Akshay Patil on 21-11-2015.
+ * This class is used to display a list view of events
  */
 public class EventListViewActivity extends Activity {
     // Progress Dialog
@@ -60,55 +61,15 @@ public class EventListViewActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_list);
-        new LoadAllProducts().execute();
-
-        list = (ListView) findViewById(R.id.events_listview);
-        listviewadapter = new ListViewAdapter(EventListViewActivity.this, R.layout.event_item,
-                eventList);
-        list.setAdapter(listviewadapter);
-
-        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                //declaring and instantiating a new intent to redirect to a new activity
-                Intent intent = new Intent(EventListViewActivity.this, EventDetailActivity.class);
-
-                //sending the quote detail like image and quote to the new activity
-                Event eventDetail = listviewadapter.getItem(position);
-
-                intent.putExtra("eName", eventDetail.getEventName());
-                intent.putExtra("eDate", eventDetail.getEventDate());
-                intent.putExtra("eTime", eventDetail.getEventTime());
-                intent.putExtra("eDesc", eventDetail.getEventDescription());
-                intent.putExtra("eLoc", eventDetail.getEventLocation());
-                //starting the new activity
-                startActivity(intent);
-
-
-            }
-        });
-    }
-
-    // Response from Edit Product Activity
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        // if result code 100
-        if (resultCode == 100) {
-            // if result code 100 is received
-            // means user edited/deleted product
-            // reload this screen again
-            Intent intent = getIntent();
-            finish();
-            startActivity(intent);
-        }
+        new LoadAllEvents().execute();
 
     }
+
 
     /**
      * Background Async Task to Load all product by making HTTP Request
      * */
-    class LoadAllProducts extends AsyncTask<String, String, String> {
+    class LoadAllEvents extends AsyncTask<String, String, String> {
 
         /**
          * Before starting background thread Show Progress Dialog
@@ -133,18 +94,18 @@ public class EventListViewActivity extends Activity {
             JSONObject json = jParser.makeHttpRequest("http://"+getResources().getString(R.string.serverIP)+"/IITEvents/getAllEvents.php", "GET", params);
 
             // Check your log cat for JSON reponse
-            Log.d("All Products: ", json.toString());
+            Log.d("All Events: ", json.toString());
 
             try {
                 // Checking for SUCCESS TAG
                 boolean success = json.getBoolean(TAG_ERROR);
 
                 if (!success) {
-                    // products found
-                    // Getting Array of Products
+                    // Events found
+                    // Getting Array of Events
                     events = json.getJSONArray(TAG_EVENTS);
 
-                    // looping through All Products
+                    // looping through All Events
                     for (int i = 0; i < events.length(); i++) {
                         JSONObject c = events.getJSONObject(i);
 
@@ -169,13 +130,7 @@ public class EventListViewActivity extends Activity {
 
                     }
                 } else {
-                    /*// no products found
-                    // Launch Add New product Activity
-                    Intent i = new Intent(getApplicationContext(),
-                            NewProductActivity.class);
-                    // Closing all previous activities
-                    i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                    startActivity(i);*/
+
                 }
             } catch (JSONException e) {
                 e.printStackTrace();
@@ -190,6 +145,33 @@ public class EventListViewActivity extends Activity {
         protected void onPostExecute(String file_url) {
             // dismiss the dialog after getting all products
             pDialog.dismiss();
+
+            list = (ListView) findViewById(R.id.events_listview);
+            listviewadapter = new ListViewAdapter(EventListViewActivity.this, R.layout.event_item,
+                    eventList);
+            list.setAdapter(listviewadapter);
+
+            list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    //declaring and instantiating a new intent to redirect to a new activity
+                    Intent intent = new Intent(EventListViewActivity.this, EventDetailActivity.class);
+
+                    //sending the quote detail like image and quote to the new activity
+                    Event eventDetail = listviewadapter.getItem(position);
+                    String eventID = "" + eventDetail.getEventID();
+                    intent.putExtra("eName", eventDetail.getEventName());
+                    intent.putExtra("eDate", eventDetail.getEventDate());
+                    intent.putExtra("eTime", eventDetail.getEventTime());
+                    intent.putExtra("eDesc", eventDetail.getEventDescription());
+                    intent.putExtra("eLoc", eventDetail.getEventLocation());
+                    intent.putExtra("eID", eventID);
+                    //starting the new activity
+                    startActivity(intent);
+
+
+                }
+            });
 
 
         }
